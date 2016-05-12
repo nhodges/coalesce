474e117a34a6537f009b7fd31e4cbc3ab272b283
Nuri Hodges <nuri@irunbackwards.com>
2016-05-12 16:21:12 -0700

    1; Implement MongoDB connection.

--- config/connections.js
+++ config/connections.js
@@ -56,14 +56,11 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  mongodb: {
+    adapter: 'sails-mongo',
+    migrate: 'safe',
+    url: process.env.MONGODB_URI
+  },
 
   /***************************************************************************
   *                                                                          *
