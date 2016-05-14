5649d7d3fbc896612abc63f9d4800be275e9f48c
Nuri Hodges <nuri@irunbackwards.com>
2016-05-14 08:23:40 -0700

    #3; Homepage updates.

--- assets/app/app.coverslider.js
+++ assets/app/app.coverslider.js
@@ -20,13 +20,41 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         execute: function() {
             AppCoverslider = class AppCoverslider {
                 constructor() {
-                    this.page = 1;
+                    this.pageno = 0;
+                    this.pages = [
+                        {
+                            image: '/images/landingpage-sustainableagriculture.jpg',
+                            heading: 'Sustainable Agriculture',
+                            description: 'We can do it.'
+                        },
+                        {
+                            image: '/images/landingpage-soupkitchen.jpg',
+                            heading: 'World Hunger',
+                            description: 'We can end it.'
+                        },
+                        {
+                            image: '/images/landingpage-animalcruelty.jpg',
+                            heading: 'Animal Cruelty',
+                            description: 'We can stop it.'
+                        }
+                    ];
                 }
             };
             AppCoverslider = __decorate([
                 core_1.Component({
                     selector: 'coverslider',
-                    template: '<a class="coverslider__nav coverslider__nav--left" (click)="page = page - 1" [hidden]="page == 1">&lt;</a><div class="coverpage" [class.current]="page == 1">{{ muffin }}</div><div class="coverpage" style="background-image: url(/images/landingpage-soupkitchen.jpg);" [class.current]="page == 2"></div><a class="coverslider__nav coverslider__nav--right" (click)="page = page + 1" [hidden]="page == 2">&gt;</a>'
+                    template: `
+      <a class="coverslider__nav coverslider__nav--left" (click)="pageno = pageno - 1" [hidden]="pageno == 0">&lt;</a>
+      <div *ngFor="let page of pages; let i = index" class="coverpage" [ngStyle]="{'background-image': 'url(' + page.image + ')'}" [class.current]="pageno == i">
+        <div class="table">
+          <div class="table__cell table__cell--vmiddle">
+            <h1>{{ page.heading }}</h1>
+            <h3>{{ page.description }}</h3>
+          </div>
+        </div>
+      </div>
+      <a class="coverslider__nav coverslider__nav--right" (click)="pageno = pageno + 1" [hidden]="pageno == pages.length - 1">&gt;</a>'
+    `
                 }), 
                 __metadata('design:paramtypes', [])
             ], AppCoverslider);
--- assets/app/app.coverslider.ts
+++ assets/app/app.coverslider.ts
@@ -1,8 +1,36 @@
 import {Component} from 'angular2/core';
 @Component({
     selector: 'coverslider',
-    template: '<a class="coverslider__nav coverslider__nav--left" (click)="page = page - 1" [hidden]="page == 1">&lt;</a><div class="coverpage" [class.current]="page == 1">{{ muffin }}</div><div class="coverpage" style="background-image: url(/images/landingpage-soupkitchen.jpg);" [class.current]="page == 2"></div><a class="coverslider__nav coverslider__nav--right" (click)="page = page + 1" [hidden]="page == 2">&gt;</a>'
+    template: `
+      <a class="coverslider__nav coverslider__nav--left" (click)="pageno = pageno - 1" [hidden]="pageno == 0">&lt;</a>
+      <div *ngFor="let page of pages; let i = index" class="coverpage" [ngStyle]="{'background-image': 'url(' + page.image + ')'}" [class.current]="pageno == i">
+        <div class="table">
+          <div class="table__cell table__cell--vmiddle">
+            <h1>{{ page.heading }}</h1>
+            <h3>{{ page.description }}</h3>
+          </div>
+        </div>
+      </div>
+      <a class="coverslider__nav coverslider__nav--right" (click)="pageno = pageno + 1" [hidden]="pageno == pages.length - 1">&gt;</a>'
+    `
 })
 export class AppCoverslider {
-  page = 1
+  pageno = 0;
+  pages = [
+    {
+      image: '/images/landingpage-sustainableagriculture.jpg',
+      heading: 'Sustainable Agriculture',
+      description: 'We can do it.'
+    },
+    {
+      image: '/images/landingpage-soupkitchen.jpg',
+      heading: 'World Hunger',
+      description: 'We can end it.'
+    },
+    {
+      image: '/images/landingpage-animalcruelty.jpg',
+      heading: 'Animal Cruelty',
+      description: 'We can stop it.'
+    }
+  ]
 }
